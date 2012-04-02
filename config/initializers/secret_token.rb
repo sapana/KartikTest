f8fc110031bd9dfc8b6783ed97475fb3387513e2
sapana <kartikmittal@gmail.com>
2012-04-01 20:27:20 -0400

    Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KartikTest::Application.config.secret_token = '85bafc0fc195a390a473bfcd4639eb51155992b5c85421148962773df1354c1fc4f31ccf064e72a7a3772a446356948ac6ec083eec099ab217c15dffc0b42203'
